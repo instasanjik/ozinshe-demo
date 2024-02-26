a5af2f1ad1b07cd71b89acbcd4aee19357a3df8d
instasanjik <koshkarbayev.07@gmail.com>
2024-02-26 17:55:54 +0600

    init: start project

---
+++ ozinshe-demo-sanzhar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ozinshe-demo-sanzhar
+//
+//  Created by Sanzhar Koshkarbayev on 26.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
