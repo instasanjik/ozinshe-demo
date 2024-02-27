343d0c54e16a30af7f95f27ebd085b9a26dabe51
instasanjik <koshkarbayev.07@gmail.com>
2024-02-27 18:39:43 +0600

    feat (style): add custom button class

--- ozinshe-demo-sanzhar/Style/OZButton.swift
+++ ozinshe-demo-sanzhar/Style/OZButton.swift
@@ -6,7 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class OZButton: UIButton {
     
+    var titleText: String? {
+        didSet {
+            setTitle(titleText, for: .normal)
+            setTitleColor(UIColor.white, for: .normal)
+            titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        }
+    }
+    
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupUI()
+    }
+
+    func setupUI() {
+        clipsToBounds = true
+        layer.cornerRadius = 12
+        backgroundColor = Style.Colors.purple
+        snp.makeConstraints { make in
+            make.height.equalTo(56)
+            make.width.equalTo(Screen.width - (24 * 2))
+        }
+    }
 }
