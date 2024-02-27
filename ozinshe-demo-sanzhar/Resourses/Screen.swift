4f2891793d182e05432a711e39aa4c54f3093b12
instasanjik <koshkarbayev.07@gmail.com>
2024-02-27 17:38:35 +0600

    feat (resourses): add screen class

--- ozinshe-demo-sanzhar/Resourses/Screen.swift
+++ ozinshe-demo-sanzhar/Resourses/Screen.swift
@@ -5,4 +5,20 @@
 //  Created by Sanzhar Koshkarbayev on 27.02.2024.
 //
 
-import Foundation
+import UIKit
+
+
+
+public enum ScreenSize {
+
+    public static let width: CGFloat = {
+        return UIScreen.main.bounds.width
+    }()
+    
+    public static let height: CGFloat = {
+        return UIScreen.main.bounds.height
+    }()
+}
+
+public let Screen = ScreenSize.self
+
