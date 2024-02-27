23615270a811e3e332a95976475a5ac043cb2fde
instasanjik <koshkarbayev.07@gmail.com>
2024-02-27 14:13:16 +0600

    feat (sing-in-module): add welcome label

--- ozinshe-demo-sanzhar/SignInViewControllers/CarouselViewController.swift
+++ ozinshe-demo-sanzhar/SignInViewControllers/CarouselViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class CarouselViewController: UIViewController {
+    
+    let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = Style.Colors.label
+        label.text = NSLocalizedString("carouselWelcomeText", comment: "ÖZINŞE-ге қош келдің!")
+        label.isExclusiveTouch = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = Style.Colors.background
+        
+        addWelcomeLabel()
+        
     }
     
-
+    
 }
 
+
+extension CarouselViewController {
+    
+    private func addWelcomeLabel() {
+        view.addSubview(welcomeLabel)
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(310)
+        }
+    }
+    
+    
+}
