b6afeef589b2c5716a879e533892cdb2bfc92f78
instasanjik <koshkarbayev.07@gmail.com>
2024-03-17 20:06:34 +0600

    init (series-scene): start task

---
+++ ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeriesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SeriesViewController.swift
+//  ozinshe-demo-sanzhar
+//
+//  Created by Sanzhar Koshkarbayev on 17.03.2024.
+//
+
+import UIKit
+
+class SeriesViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
