0ab3d92e077cebc817c5ce85eb1b67cace1f5158
instasanjik <koshkarbayev.07@gmail.com>
2024-03-17 20:19:49 +0600

    feat (series-scene): create season collection view cell

---
+++ ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeasonCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SeasonCollectionViewCell.swift
+//  ozinshe-demo-sanzhar
+//
+//  Created by Sanzhar Koshkarbayev on 17.03.2024.
+//
+
+import UIKit
+
+class SeasonCollectionViewCell: UICollectionViewCell {
+    
+}
