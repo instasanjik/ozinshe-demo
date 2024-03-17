516047dfccf3d5cedf11e71c8bd6798a0f2a9733
instasanjik <koshkarbayev.07@gmail.com>
2024-03-17 20:21:07 +0600

    feat (series-scene): create series table view cell

---
+++ ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeriesTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SeriesTableViewCell.swift
+//  ozinshe-demo-sanzhar
+//
+//  Created by Sanzhar Koshkarbayev on 17.03.2024.
+//
+
+import UIKit
+
+class SeriesTableViewCell: UITableViewCell {
+    
+    static let ID: String = "SeriesTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
