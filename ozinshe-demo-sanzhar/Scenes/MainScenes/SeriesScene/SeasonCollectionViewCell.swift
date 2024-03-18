4194dec0993f2d44e8d8027a273583e1cf41244f
instasanjik <koshkarbayev.07@gmail.com>
2024-03-18 09:05:29 +0600

    feat (series-scene): add seasons collection view

--- ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeasonCollectionViewCell.swift
+++ ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeasonCollectionViewCell.swift
@@ -6,17 +6,54 @@
 //
 
 import UIKit
+import SnapKit
+import SkeletonView
 
 class SeasonCollectionViewCell: UICollectionViewCell {
     
     static let ID: String = "SeasonCollectionViewCell"
     
+    
+    lazy var seasonLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 сезон"
+        label.font = .systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = Style.StaticColors.label
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            self.backgroundColor = isSelected ? Style.Colors.purple400 : Style.Colors.gray700
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        self.backgroundColor = Style.Colors.gray700
+        self.layer.cornerRadius = 8
+        
+        setupSeasonLabel()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+}
+
+
+extension SeasonCollectionViewCell {
+    
+    fileprivate func setupSeasonLabel() {
+        self.addSubview(seasonLabel)
+        
+        seasonLabel.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.top.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
     
 }
--- ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeriesViewController.swift
+++ ozinshe-demo-sanzhar/Scenes/MainScenes/SeriesScene/SeriesViewController.swift
@@ -11,42 +11,118 @@ import SkeletonView
 
 class SeriesViewController: UIViewController {
     
+    var selectedSeasonIndexPath: IndexPath = IndexPath(item: 0, section: 0)
+    
     lazy var seasonsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
-        layout.itemSize = CGSizeMake(300, 240)
         layout.scrollDirection = .horizontal
         
-        let collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: layout)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.dataSource = self
         collectionView.delegate = self
         
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = .clear
+        collectionView.allowsSelection = true
         
-        collectionView.register(RecomendationCollectionViewCell.self,
-                                forCellWithReuseIdentifier: RecomendationCollectionViewCell.ID)
+        collectionView.register(SeasonCollectionViewCell.self,
+                                forCellWithReuseIdentifier: SeasonCollectionViewCell.ID)
         
         collectionView.isSkeletonable = true
         return collectionView
+    }()
+    
+    lazy var seriesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.backgroundColor = .clear
+//        tableView.separatorStyle = .none
+        
+        tableView.register(SeriesTableViewCell.self,
+                           forCellReuseIdentifier: SeriesTableViewCell.ID)
+        
+        return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = Style.Colors.background
+        
+        setupSeasonsCollectionView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        seasonsCollectionView.selectItem(at: selectedSeasonIndexPath, animated: true, scrollPosition: .top)
     }
     
     
 }
 
-extension SeriesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+extension SeriesViewController {
+    
+    fileprivate func setupSeasonsCollectionView() {
+        view.addSubview(seasonsCollectionView)
+        
+        seasonsCollectionView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.height.equalTo(34)
+            make.top.equalToSuperview().inset(100)
+        }
+    }
+    
+    
+}
+
+
+extension SeriesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SeasonCollectionViewCell.ID, for: indexPath)
+        if indexPath == selectedSeasonIndexPath {
+            cell.isSelected = true
+            Logger.log(.success, "\(indexPath)")
+        } else {
+            cell.isSelected = false
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let label = UILabel(frame: CGRect.zero)
+        label.text = "1 сезон"
+        label.font = .systemFont(ofSize: 12, weight: .semibold)
+        label.sizeToFit()
+        return CGSize(width: label.frame.width + (16 * 2), height: 34)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if selectedSeasonIndexPath != indexPath {
+            selectedSeasonIndexPath = indexPath
+        }
+    }
+    
+    
+}
+
+
+extension SeriesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         <#code#>
     }
     
