107902110f1786b51f3ac33986fa96314c7df12f
instasanjik <koshkarbayev.07@gmail.com>
2024-04-09 22:39:49 +0500

    feat (common): add personal data page localization

--- ozinshe-demo-sanzhar/Common/Files/StaticData.swift
+++ ozinshe-demo-sanzhar/Common/Files/StaticData.swift
@@ -46,6 +46,8 @@ struct StaticData {
         return "English"
     }
     
+    static let personalDataCell: [String] = ["PersonalData-name", "PersonalData-birhdayDate", "PersonalData-phoneNumber"]
+    
     static let termOfUse: String = """
         What is Lorem Ipsum?
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
