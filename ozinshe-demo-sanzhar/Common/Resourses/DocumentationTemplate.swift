d15858c5d29881bd5cb846baa64febb11bb6fd84
instasanjik <koshkarbayev.07@gmail.com>
2024-04-13 20:16:47 +0500

    docs (common): add documentation template

---
+++ ozinshe-demo-sanzhar/Common/Resourses/DocumentationTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentationTemplate.swift
+//  ozinshe-demo-sanzhar
+//
+//  Created by Sanzhar Koshkarbayev on 13.04.2024.
+//
+
+// MARK: - Internal variables
+// MARK: - External variables
+// MARK: - UI Elements
+// MARK: - View Life Cycle
+// MARK: - UI Setups
+// MARK: - Targets
+// MARK: - Internal functions
+// MARK: - External functions
+// MARK: - Delegates
+
