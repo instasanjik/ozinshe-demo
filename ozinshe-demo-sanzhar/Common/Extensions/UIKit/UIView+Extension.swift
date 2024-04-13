e1e26848fae3c72ae6c8ebcbcc3bff9229029086
instasanjik <koshkarbayev.07@gmail.com>
2024-04-13 23:38:52 +0500

    docs (uiview-extension): add docs

--- ozinshe-demo-sanzhar/Common/Extensions/UIKit/UIView+Extension.swift
+++ ozinshe-demo-sanzhar/Common/Extensions/UIKit/UIView+Extension.swift
@@ -7,20 +7,26 @@
 
 import UIKit
 
+/// Extension to UIView providing a method for setting the view's height with optional animation.
 extension UIView {
-    func setHeight(_ h:CGFloat, animateTime:TimeInterval?=nil) {
-
-        if let c = self.constraints.first(where: { $0.firstAttribute == .height && $0.relation == .equal }) {
-            c.constant = CGFloat(h)
-
+    
+    /// Sets the height of the view to the specified value.
+    ///
+    /// - Parameters:
+    ///   - height: The new height for the view.
+    ///   - animateTime: The duration of the animation, if animation is desired. If set to nil, changes will be immediate.
+    func setHeight(_ height: CGFloat, animateTime: TimeInterval? = nil) {
+        if let heightConstraint = self.constraints.first(where: { $0.firstAttribute == .height && $0.relation == .equal }) {
+            heightConstraint.constant = height
+            
             if let animateTime = animateTime {
-                UIView.animate(withDuration: animateTime, animations:{
+                UIView.animate(withDuration: animateTime) {
                     self.superview?.layoutIfNeeded()
-                })
-            }
-            else {
+                }
+            } else {
                 self.superview?.layoutIfNeeded()
             }
         }
     }
+    
 }
