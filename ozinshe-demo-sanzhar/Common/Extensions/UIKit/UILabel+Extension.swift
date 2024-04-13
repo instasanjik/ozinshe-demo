5f42bfc62d9c8e9cb4335a0798348d981f465e7c
instasanjik <koshkarbayev.07@gmail.com>
2024-04-13 23:40:14 +0500

    docs (uilabel-extension): add docs

--- ozinshe-demo-sanzhar/Common/Extensions/UIKit/UILabel+Extension.swift
+++ ozinshe-demo-sanzhar/Common/Extensions/UIKit/UILabel+Extension.swift
@@ -7,67 +7,22 @@
 
 import UIKit
 
+/// Extension to UILabel providing utility properties for counting lines and checking truncation.
 extension UILabel {
-
-    func applyGradientWith(startColor: UIColor, endColor: UIColor) -> Bool {
-
-        var startColorRed:CGFloat = 0
-        var startColorGreen:CGFloat = 0
-        var startColorBlue:CGFloat = 0
-        var startAlpha:CGFloat = 0
-
-        if !startColor.getRed(&startColorRed, green: &startColorGreen, blue: &startColorBlue, alpha: &startAlpha) {
-            return false
-        }
-
-        var endColorRed:CGFloat = 0
-        var endColorGreen:CGFloat = 0
-        var endColorBlue:CGFloat = 0
-        var endAlpha:CGFloat = 0
-
-        if !endColor.getRed(&endColorRed, green: &endColorGreen, blue: &endColorBlue, alpha: &endAlpha) {
-            return false
-        }
-
-        let gradientText = self.text ?? ""
-
-        let name:String = NSAttributedString.Key.font
-        let textSize: CGSize = gradientText.size(attributes: [name:self.font])
-        let width:CGFloat = textSize.width
-        let height:CGFloat = textSize.height
-
-        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
-
-        guard let context = UIGraphicsGetCurrentContext() else {
-            UIGraphicsEndImageContext()
-            return false
-        }
-
-        UIGraphicsPushContext(context)
-
-        let glossGradient:CGGradient?
-        let rgbColorspace:CGColorSpace?
-        let num_locations:size_t = 2
-        let locations:[CGFloat] = [ 0.0, 1.0 ]
-        let components:[CGFloat] = [startColorRed, startColorGreen, startColorBlue, startAlpha, endColorRed, endColorGreen, endColorBlue, endAlpha]
-        rgbColorspace = CGColorSpaceCreateDeviceRGB()
-        glossGradient = CGGradient(colorSpace: rgbColorspace!, colorComponents: components, locations: locations, count: num_locations)
-        let topCenter = CGPoint.zero
-        let bottomCenter = CGPoint(x: 0, y: textSize.height)
-        context.drawLinearGradient(glossGradient!, start: topCenter, end: bottomCenter, options: CGGradientDrawingOptions.drawsBeforeStartLocation)
-
-        UIGraphicsPopContext()
-
-        guard let gradientImage = UIGraphicsGetImageFromCurrentImageContext() else {
-            UIGraphicsEndImageContext()
-            return false
-        }
-
-        UIGraphicsEndImageContext()
-
-        self.textColor = UIColor(patternImage: gradientImage)
-
-        return true
+    
+    /// Returns the number of lines currently rendered in the label.
+    var countLabelLines: Int {
+        let text = self.text! as NSString
+        let attributes = [NSAttributedString.Key.font : self.font]
+        
+        let labelSize = text.boundingRect(with: CGSize(width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: attributes as [NSAttributedString.Key : Any], context: nil)
+        return Int(ceil(CGFloat(labelSize.height) / self.font.lineHeight))
     }
-
+    
+    /// Returns a Boolean value indicating whether the label's text is truncated.
+    var isTruncated: Bool {
+        guard numberOfLines > 0 else { return false }
+        return countLabelLines > numberOfLines
+    }
+    
 }
