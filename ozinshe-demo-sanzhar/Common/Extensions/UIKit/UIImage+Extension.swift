8e988c38b77f07452c3e33a808b4109af5589b50
instasanjik <koshkarbayev.07@gmail.com>
2024-04-13 23:41:20 +0500

    docs (uiimage-extension): add docs

--- ozinshe-demo-sanzhar/Common/Extensions/UIKit/UIImage+Extension.swift
+++ ozinshe-demo-sanzhar/Common/Extensions/UIKit/UIImage+Extension.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 
+/// Extension to UIImage providing utility methods for creating images.
 extension UIImage {
     
+    /// Creates a circular image with the specified diameter and color.
+    ///
+    /// - Parameters:
+    ///   - diameter: The diameter of the circular image.
+    ///   - color: The color to fill the circle with.
+    /// - Returns: A circular UIImage with the specified diameter and color.
     static func circle(diameter: CGFloat, color: UIColor) -> UIImage {
         let size = CGSize(width: diameter, height: diameter)
         let rect = CGRect(origin: .zero, size: size)
