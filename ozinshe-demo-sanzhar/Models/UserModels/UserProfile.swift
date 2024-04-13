8e425717d9a0d587d5ee9427c30212f1eda0cbf9
instasanjik <koshkarbayev.07@gmail.com>
2024-04-13 18:42:52 +0500

    fix (models): update json intialization for user profile model

--- ozinshe-demo-sanzhar/Models/UserModels/UserProfile.swift
+++ ozinshe-demo-sanzhar/Models/UserModels/UserProfile.swift
@@ -17,19 +17,22 @@ class UserProfile {
     var birthDate: String = ""
     var language: String = ""
     
-    init(id: Int, name: String, phoneNumber: String, birtdayDate: String) {
+    init() { }
+    
+    init(id: Int, name: String, phoneNumber: String, birtdayDate: String, email: String) {
         self.id = id
         self.name = name
         self.phoneNumber = phoneNumber
         self.birthDate = birtdayDate
+        self.email = email
     }
     
     init(json: JSON) {
         if let temp = json["id"].int                    { self.id = temp }
         if let temp = json["user"]["email"].string      { self.email = temp }
-        if let temp = json["phoneNumber"].string        { self.name = temp }
+        if let temp = json["phoneNumber"].string        { self.phoneNumber = temp }
         if let temp = json["name"].string               { self.name = temp }
-        if let temp = json["birtdayDate"].string        { self.birthDate = temp }
+        if let temp = json["birthDate"].string          { self.birthDate = temp }
         if let temp = json["language"].string           { self.language = temp }
     }
     
