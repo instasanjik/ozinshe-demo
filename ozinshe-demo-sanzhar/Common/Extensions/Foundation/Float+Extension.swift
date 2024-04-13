f45172a32cc93e4c92c0f3ab51f2cf34bd3ccd69
instasanjik <koshkarbayev.07@gmail.com>
2024-04-13 23:43:25 +0500

    docs (float-extension): add docs

--- ozinshe-demo-sanzhar/Common/Extensions/Foundation/Float+Extension.swift
+++ ozinshe-demo-sanzhar/Common/Extensions/Foundation/Float+Extension.swift
@@ -9,16 +9,20 @@ import Foundation
 
 extension Float {
     
+    /// Returns a formatted string representing the time interval.
+    ///
+    /// - Returns: A string representing the time interval in the format "HH:mm:ss" if hours are present, otherwise "mm:ss".
     var timeCodeValue: String {
-            let hours = Int(self) / 3600
-            let minutes = (Int(self) % 3600) / 60
-            let seconds = Int(self) % 60
-            
-            if hours > 0 {
-                return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-            } else {
-                return String(format: "%02d:%02d", minutes, seconds)
+        let hours = Int(self) / 3600
+        let minutes = (Int(self) % 3600) / 60
+        let seconds = Int(self) % 60
+        
+        if hours > 0 {
+            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            return String(format: "%02d:%02d", minutes, seconds)
         }
     }
     
 }
+
